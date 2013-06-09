b694e3f7a3b9f80edc9fb699679c301e2c3b6ae5
David Scott <dave.scott@eu.citrix.com>
2013-06-09 12:33:00 +0000

    Add replace.file(filename, new contents)
    This function first backs up the existing file and then atomically
    replaces it.
    
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

---
+++ replace.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os, os.path, sys, subprocess
+
+def choose_filename(name, ext):
+	# choose a name for the backup file that doesn't exist yet
+	i = 0
+	stem = name + "." + ext
+	filename = stem
+	while os.path.exists(filename):
+		filename = stem + "." + str(i)
+		i = i + 1
+	return filename
+
+def cp(existing, new):
+	cmd = [ "/bin/cp", "-f", existing, new ]
+	x = subprocess.call(cmd)
+	if x <> 0:
+		print >>sys.stderr, "FAILED: to create backup file (%s)" % (" ".join(cmd))
+		exit(1)
+
+def write(filename, contents):
+	f = open(filename, "w")
+	try:
+		for line in contents:
+			f.write(line)
+			f.write("\n")
+	finally:
+		f.close()
+
+def file(name, contents):
+	backup = choose_filename(name, "backup")
+	cp(name, backup)
+	print >>sys.stderr, "OK: saved backup of %s in %s" % (name, backup)
+	new = choose_filename(name, "new")
+	write(new, contents)
+	os.rename(new, name)
+
+		

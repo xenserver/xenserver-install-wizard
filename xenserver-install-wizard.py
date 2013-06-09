9018061e793332eab14188fec60345b515b52308
David Scott <dave.scott@eu.citrix.com>
2013-06-09 12:44:47 +0000

    Actually replace files and really reboot the system.
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- xenserver-install-wizard.py
+++ xenserver-install-wizard.py
@@ -1,31 +1,28 @@
 #!/usr/bin/env python
 
 import sys, subprocess
-import tui, grub, networking, iptables, storage
-
-def replace_file(name, lines):
-	print "Create a backup of %s" % name
-	print "Replace %s with:" % name
-	for line in lines:
-		print line
-
+import replace, tui, grub, networking, iptables, storage
 
 def reboot():
 	print >>sys.stderr, "Triggering an immediate reboot"
+	cmd = [ "/sbin/reboot" ]
+	x = subprocess.call(cmd)
+	if x <> 0:
+		print >>sys.stderr, "FAILED: to trigger a reboot (%s)" % (" ".join(cmd))
 
 if __name__ == "__main__":
 	need_to_reboot = False
 	r = grub.analyse()
 	if r:
 		need_to_reboot = True
-		replace_file(r[0], r[1])
+		replace.file(r[0], r[1])
 	r = networking.analyse()
 	if r:
 		for change in r:
-			replace_file(change[0], change[1])
+			replace.file(change[0], change[1])
 	r = iptables.analyse()
 	if r:
-		replace_file(r[0], r[1])
+		replace.file(r[0], r[1])
 		iptables.restart()
 	storage.analyse()
 	print "Welcome to XenServer!"

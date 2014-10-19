eaac9bf9c6f284c0fb4e559cb385deaa95e577ca
David Scott <dave.scott@eu.citrix.com>
2014-10-19 17:53:51 +0000

    Debian/Ubuntu: don't fail if 'chkconfig' doesn't exist
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- xenserver-install-wizard.py
+++ xenserver-install-wizard.py
@@ -20,17 +20,18 @@ def stop_xend(tui):
 	need_to_reboot = False
 
 	print >>sys.stderr, "Permanently stopping xend"
-	if subprocess.call(["chkconfig", "xend"]) <> 0:
-		if subprocess.call(["chkconfig", "--level", "345", "xend", "off"]) <> 0:
-			print >>sys.stderr, "FAILED: to disable xend"
-	elif os.path.exists(toolstack.etc_default_xen):
+	try:
+		if subprocess.call(["chkconfig", "xend"]) <> 0:
+			if subprocess.call(["chkconfig", "--level", "345", "xend", "off"]) <> 0:
+				print >>sys.stderr, "FAILED: to disable xend"
+	except:
+		pass # no chkconfig on Debian/Ubuntu
+	if os.path.exists(toolstack.etc_default_xen):
 		r = toolstack.analyse(tui)
 		if r:
 			need_to_reboot = True
 			for change in r:
 				replace.file(change[0], change[1])
-	else: 
-		print >>sys.stderr, "FAILED: don't know how to disable xend"
 	if subprocess.call(["service", "xend", "stop"]) <> 0:
 		print >>sys.stderr, "FAILED: to stop xend"
 	return need_to_reboot
